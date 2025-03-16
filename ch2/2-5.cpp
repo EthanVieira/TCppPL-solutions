10e820e1e64f64d0c699a79fc2187af8e5bf2c9c
Ethan Vieira <ethanvieira@gmail.com>
2025-03-16 10:58:23 -0500

    Create 2-5.cpp

---
+++ ch2/2-5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    bool b = true;
+    char c = 'c';
+    int i = 2;
+    double d = 5.55;
+    std::string s = "hello";
+    std::cout << b << "\n";
+    std::cout << c << "\n";
+    std::cout << i << "\n";
+    std::cout << d << "\n";
+    std::cout << s << "\n";
+}
