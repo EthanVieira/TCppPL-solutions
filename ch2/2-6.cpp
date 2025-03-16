c7c9be27ba135835943c0b65cee7df8128216f37
Ethan Vieira <ethanvieira@gmail.com>
2025-03-16 11:05:29 -0500

    create 2-6.cpp

---
+++ ch2/2-6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    bool b = true;
+    char c = 0;
+    int i = 0;
+    double d = 0;
+    std::string s = "";
+    std::cout << "Enter a bool:\n";
+    std::cin >> b;
+    std::cout << "Enter a char:\n";
+    std::cin >> c;
+    std::cout << "Enter an int:\n";
+    std::cin >> i;
+    std::cout << "Enter a double:\n";
+    std::cin >> d;
+    std::cout << "Enter a string:\n";
+    std::cin >> s;
+}
